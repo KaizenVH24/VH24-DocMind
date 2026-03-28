fde654becae11c2a32ec97950c09e43b40ef4776
Vinay Hulsurkar <186893860+KaizenVH24@users.noreply.github.com>
2026-03-28 15:40:52 +0530

    Delete test_loader.py

--- test_loader.py
+++
@@ -1,7 +0,0 @@
-from app.document_loader import ingest_file
-
-chunks = ingest_file("data/sample.txt")
-
-for i, chunk in enumerate(chunks[:3]):
-    print("\n--- Chunk", i, "---")
-    print(chunk.page_content[:200])
